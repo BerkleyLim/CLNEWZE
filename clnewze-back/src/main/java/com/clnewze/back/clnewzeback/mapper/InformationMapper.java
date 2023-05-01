5db0f99f1161fa621850f407bae4e8709fff4955
berkleylim <berkleylim16@gmail.com>
2023-05-01 22:50:46 +0900

    test: spring 연동 테스트
    information 부분

--- clnewze-back/src/main/java/com/clnewze/back/clnewzeback/mapper/InformationMapper.java
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/mapper/InformationMapper.java
@@ -1,10 +1,15 @@
 package com.clnewze.back.clnewzeback.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import com.clnewze.back.clnewzeback.domain.Information;
 
 @Repository
 @Mapper
 public interface InformationMapper {
-  public Integer totSize(String findStr);
+  // public Integer totSize(String findStr);
+  public List<Information> selectList();
 }
---
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/service/InformationService.java
@@ -0,0 +1,19 @@
+package com.clnewze.back.clnewzeback.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clnewze.back.clnewzeback.domain.Information;
+import com.clnewze.back.clnewzeback.mapper.InformationMapper;
+
+@Service
+public class InformationService {
+  @Autowired
+  InformationMapper informationMapper;
+
+  public List<Information> list() {
+    return informationMapper.selectList();
+  }
+}
