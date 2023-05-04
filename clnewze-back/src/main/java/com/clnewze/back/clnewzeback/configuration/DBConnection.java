99799613b519c4a2d225b174c4b34787fdfec621
berkleylim <berkleylim16@gmail.com>
2023-05-04 23:24:41 +0900

    style: my-batis 연동 시도

--- clnewze-back/src/main/java/com/clnewze/back/clnewzeback/configuration/DBConnection.java
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/configuration/DBConnection.java
@@ -21,8 +21,25 @@ import com.zaxxer.hikari.HikariConfig;
 @Configuration
 @MapperScan(value = {
     "com.clnewze.back.clnewzeback.mapper"
-}, sqlSessionFactoryRef = "db1SqlSessionFactory")
+}, sqlSessionFactoryRef = "sqlSessionFactory")
 public class DBConnection {
+
+  /*
+   * mybatis:
+   * type-aliases-package: com.clnewze.back.clnewzeback.mapper
+   * configuration:
+   * cache-enabled: true
+   * use-column-label: true
+   * use-generated-keys: false
+   * map-underscore-to-camel-case: true
+   * default-statement-timeout: 25000
+   * jdbc-type-for-null: NULL
+   * mapper-locations: classpath:mapper/*Mapper.xml
+   */
+  @Value(value = "${mybatis.type-aliases-package}")
+  String location;
+  @Value(value = "${mybatis.mapper-locations}")
+  String locationPattern;
 
   // 기본으로 사용할 데이터소스에 @Primary를 넣어줘야함
   @Primary
@@ -49,17 +66,17 @@ public class DBConnection {
     sqlSessionFactoryBean.setDataSource(dataSource);
     sqlSessionFactoryBean.setTypeAliasesPackage("com.clnewze.back.backclnewze.domain");
     sqlSessionFactoryBean.setConfigLocation(
-        applicationContext.getResource("classpath:/mapper/db1/db1-config.xml"));
+        applicationContext.getResource(location));
     sqlSessionFactoryBean.setMapperLocations(
-        applicationContext.getResources("classpath*:/mapper/db1/**/*Mapper.xml"));
+        applicationContext.getResources(locationPattern));
 
     return sqlSessionFactoryBean.getObject();
   }
 
   @Bean(name = "SqlSessionTemplate")
   @Primary
-  public SqlSessionTemplate db1SqlSessionTemplate(SqlSessionFactory db1SqlSessionFactory) {
-    return new SqlSessionTemplate(db1SqlSessionFactory);
+  public SqlSessionTemplate SqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
+    return new SqlSessionTemplate(sqlSessionFactory);
   }
 
 }
