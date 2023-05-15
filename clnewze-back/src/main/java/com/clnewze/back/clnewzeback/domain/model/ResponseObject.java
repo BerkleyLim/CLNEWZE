2dcbb3c4dd7c5c2c7a54f4f58470d5a5286e7eaa
berkleylim <berkleylim16@gmail.com>
2023-05-15 22:46:42 +0900

    feat: 벡엔드 서버 상태값 추가
    ResponseEntity로 이용한 HttpStatus

---
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/domain/model/ResponseObject.java
@@ -0,0 +1,26 @@
+package com.clnewze.back.clnewzeback.domain.model;
+
+import com.clnewze.back.clnewzeback.util.error.code.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+
+@Data
+public class ResponseObject<T> {
+  String message;
+  T data;
+
+  @JsonInclude(value = JsonInclude.Include.NON_NULL)
+  String errorMessage;
+
+  @JsonInclude(value = Include.NON_NULL)
+  ErrorCode errorCode;
+
+  public ResponseObject() {
+  }
+
+  public ResponseObject(String messsage) {
+    this.message = messsage;
+  }
+}
---
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/util/ResponseStatusData.java
@@ -0,0 +1,20 @@
+package com.clnewze.back.clnewzeback.util;
+
+import com.clnewze.back.clnewzeback.domain.model.ResponseObject;
+import com.clnewze.back.clnewzeback.util.error.code.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ResponseStatusData {
+  public <T> ResponseEntity<ResponseObject<T>> makesErrorMessage(ErrorCode errorCode, String errorMessage) {
+    if (errorCode.equals(ErrorCode.TRANSACTION_ROLLBACK_BY_EXCEPTION))
+      log.info(errorMessage);
+    ResponseObject<T> ro = new ResponseObject<>("Failed");
+    ro.setErrorCode(errorCode);
+    ro.setErrorMessage("Detailed Message: " + errorMessage);
+    return new ResponseEntity<>(ro, HttpStatus.EXPECTATION_FAILED);
+  }
+}
