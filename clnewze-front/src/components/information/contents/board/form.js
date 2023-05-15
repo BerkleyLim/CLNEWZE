3f141f02dcf44ec01bf2f4aa302a894b73bb802a
berkleylim <berkleylim16@gmail.com>
2023-05-15 18:34:11 +0900

    refactor: Information 수정
    모달 data값 교정
    
    null 값 체크 기능 추가

--- clnewze-front/src/components/information/contents/board/form.js
+++ clnewze-front/src/components/information/contents/board/form.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { Nav, Navbar } from "reactstrap";
 
-const BoardForm = (information) => {
+const BoardForm = ({information}) => {
   return (
     <div className="boardDetail">
       <h5 className="DetailTitle">
-        {information.artist} - {information.title}
+        {information?.artist} - {information?.title}
       </h5>
       <div style={{fontSize:"9px", color: "#555", float:"right"}}>
-        조회수 : {information.view}{" "}
+        조회수 : {information?.view}{" "}
       </div>
       <Nav className="common-content-detail-container">
         <img src="{{img}}" className="album" />
         <div className="detail-container">
-          <div className="">장르 : {information.genre}</div>
-          <div className="">발매날짜 : {information.releaseDate}년</div>
-          <div className="">앨범명 : {information.albumName}</div>
-          <div className="">음원 듣기 : {information.musicLink}</div>
+          <div className="">장르 : {information?.genre}</div>
+          <div className="">발매날짜 : {information?.releaseDate}년</div>
+          <div className="">앨범명 : {information?.albumName}</div>
+          <div className="">음원 듣기 : {information?.musicLink}</div>
         </div>
       </Nav>
 
       <h3>상세정보</h3>
 
       {/* <!-- 내용 작성 --> */}
-      <div style={{height:"80%"}}>{information.content}</div>
+      <div style={{height:"80%"}}>{information?.content}</div>
 
       {/* <!-- 여기는 하단에 놓는다 --> */}
       <Navbar className="information-button-event">
