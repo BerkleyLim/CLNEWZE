673fb2972b624d673c80b8ef7b3707626be34fd2
berkleylim <berkleylim16@gmail.com>
2023-05-15 17:02:49 +0900

    feat: Information 수정
    Back-end 수정
    
    domain 수정

--- clnewze-back/src/main/java/com/clnewze/back/clnewzeback/domain/Information.java
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/domain/Information.java
@@ -1,10 +1,21 @@
 package com.clnewze.back.clnewzeback.domain;
 
-import java.io.Serializable;
+import java.time.LocalDate;
 
 import lombok.Data;
 
 @Data
 public class Information {
-  Integer no;
+  Integer ino;
+  String artist;
+  String title;
+  String img;
+  String genre;
+  Integer viewNumber;
+  LocalDate releaseDate;
+  String contents;
+  String albumName;
+  LocalDate upLoadDate;
+  String userName;
+  String musicLink;
 }
