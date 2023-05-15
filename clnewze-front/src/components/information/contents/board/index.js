21d9e2f9a186bce3112d5c2b1ec51a55cb74c893
berkleylim <berkleylim16@gmail.com>
2023-05-15 21:13:05 +0900

    refactor: information 수정
    UI 표시 수정

--- clnewze-front/src/components/information/contents/board/index.js
+++ clnewze-front/src/components/information/contents/board/index.js
@@ -26,23 +26,26 @@ const BoardIndex = ({ boards, onClickView }) => {
                   />
                 </Col>
                 <Col md={11}>
-                  <div className="">
-                    <Row style={{ verticalAlign: "bottom" }}>
-                      <h5 className="card-title col-md-8 align-self-center">
-                        장르 : {board?.genre}
-                      </h5>
-                      <div className="display col align-self-center justify-content-end">
-                        조회수 : {board?.viewNumber}
-                      </div>
-                    </Row>
+                  <Row style={{ verticalAlign: "bottom" }}>
+                    <h5 className="card-title col-md-4 align-self-center">
+                      장르 : {board?.genre}
+                    </h5>
+                    <div className="display col-md-8 align-self-center justify-content-end">
+                      조회수 : {board?.viewNumber}
+                    </div>
+                  </Row>
+                  <Row className="card-text">
+                    <Col md={1} />● 발매날짜 : {board?.releaseDate} 년
+                  </Row>
+                  <Row className="card-text">
+                    <Col md={1} />● 앨범명 : {board?.albumName}
+                  </Row>
+                  <Row className="card-text">
+                    <Col md={1} />● 상세정보 : {board?.contents}
+                  </Row>
 
-                    <p className="card-text">
-                      ● 발매날짜 : {board?.releaseDate} 년 <br />● 앨범명 :{" "}
-                      {board?.albumName} <br />● 상세정보 : {board?.contents}
-                    </p>
-                    <p className="card-text">
-                      <small className="text-muted">{board?.upLoadDate}</small>
-                    </p>
+                  <div className="card-text">
+                    <small className="text-muted">{board?.upLoadDate}</small>
                   </div>
                 </Col>
               </Row>
