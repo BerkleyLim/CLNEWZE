19024e7f79d003b8b0288c37aa7299b1237d35ad
berkleylim <berkleylim16@gmail.com>
2023-05-16 15:37:14 +0900

    fix: 곡 정보 선택 버그 수정
    장르별 설정 데이터 여부 확인
    
    mybatis 일부 수정, requestparam으로 변경

--- clnewze-back/src/main/java/com/clnewze/back/clnewzeback/controller/InformationController.java
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/controller/InformationController.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.clnewze.back.clnewzeback.domain.Information;
@@ -24,9 +24,10 @@ public class InformationController {
 
   @GetMapping("/selectList")
   public ResponseEntity<ResponseObject<List<Information>>> selectList(
-      @RequestBody(required = false) Information information) {
+      @RequestParam(required = false) String genre) {
 
-    List<Information> result = informationService.selectList(information == null ? null : information.getGenre());
+    System.out.println(genre);
+    List<Information> result = informationService.selectList(genre);
     ResponseObject<List<Information>> ro = new ResponseObject<>("성공");
     ro.setData(result);
     return new ResponseEntity<>(ro, HttpStatus.OK);
