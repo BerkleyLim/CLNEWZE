63020de051e9ed2cbdd56ce88ca37c4529b02655
berkleylim <berkleylim16@gmail.com>
2023-05-16 17:40:14 +0900

    style: 곡정보 Back-end 필요 없는 코드 제거
    informationController 코드 정리

--- clnewze-back/src/main/java/com/clnewze/back/clnewzeback/controller/InformationController.java
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/controller/InformationController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +25,6 @@ public class InformationController {
   public ResponseEntity<ResponseObject<List<Information>>> selectList(
       @RequestParam(required = false) String genre) {
 
-    System.out.println(genre);
     List<Information> result = informationService.selectList(genre);
     ResponseObject<List<Information>> ro = new ResponseObject<>("성공");
     ro.setData(result);
