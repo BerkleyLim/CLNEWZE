097bfddbfd56611e8d4a331766fb8ebed40fa397
berkleylim <berkleylim16@gmail.com>
2023-06-09 23:12:50 +0900

    fix: Update header dropdown
    언어 선택 부분 UI 수정

--- clnewze-front/src/components/commom/header/dropdown/language/dropdown.js
+++ clnewze-front/src/components/commom/header/dropdown/language/dropdown.js
@@ -1,26 +1,22 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { Button, ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap'
+import { ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap'
 
 const LanguageDropdown = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const dispatch = useDispatch();
-  const language = useSelector(state => state.language);
+  // const language = useSelector(state => state.language);
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
   const onLanguageChange = (language) => {
     dispatch({type:"setLanguage", language:language});
   }
 
-  // console.log(language.language);
   return (
     <ButtonDropdown isOpen={dropdownOpen} direction="down" toggle={toggle}>
-      <Button outline color="btn-secondary">
-        한국어
-      </Button>
-      <DropdownToggle />
+      <DropdownToggle caret size="lg">한국어</DropdownToggle>
       <DropdownMenu>
         <DropdownItem onClick={() => onLanguageChange("ko")}>한국어</DropdownItem>
         <DropdownItem onClick={() => onLanguageChange("ja")}>Japanese</DropdownItem>
