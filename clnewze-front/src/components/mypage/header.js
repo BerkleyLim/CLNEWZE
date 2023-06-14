3e480c01740a595cfd95efb4ddb4903d53c8bfd5
berkleylim <berkleylim16@gmail.com>
2023-06-14 22:33:02 +0900

    Revert "feat: Update Mypage's Header"
    This reverts commit 2a4af99bcc0ae237ab22f4f9b8abadbadae13ddd.

--- clnewze-front/src/components/mypage/header.js
+++ clnewze-front/src/components/mypage/header.js
@@ -1,23 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./mypage.scss";
-import { Button, Input, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { Button } from "reactstrap";
 
 const MypageHeader = () => {
-
-  const [profileImg, setProfileImg] = useState("/image/file/mypage/defalut-my-profile.png");
-  // /**
-  //  * profile 업로드용 메서드
-  //  */
-  // const profileUpload = () => {};
-
-  const [isModal, setIsModal] = useState(false);
-  const modalToggle = () => setIsModal(!isModal);
-
-  /* 구독하기 */
-  const subscriptOnClick = () => {
-    setIsModal(!isModal)
-  }
   return (
     <div className="mypage-header">
       {/* <!-- 사진 --> */}
@@ -29,12 +15,8 @@ const MypageHeader = () => {
       {/* <!-- 마이페이지 정보 (커버랑 같이 겹치기) --> */}
       <div className="profile-information">
         <div className="profile-img">
-          <img
-            src={profileImg}
-            className="img-size"
-          />
+          <img src="/image/file/mypage/defalut-my-profile.png" className="img-size" />
         </div>
-        <Input type="file" style={{ width: "80px", height: "50px" }} />
         <div className="profile-component1">
           <div className="profile-name">홍길동</div>
           <div className="profile-introduction">
@@ -42,29 +24,11 @@ const MypageHeader = () => {
           </div>
         </div>
         <div className="profile-component2">
-          <div className="">
-            <Button>충전하기</Button>
-            <Button onClick={() => subscriptOnClick()}>구독하기</Button>
-          </div>
+          <div className=""><Button>충전하기</Button></div>
           <div className="profile-return">환전 포인트 : 100000 p</div>
           <div className="profile-cache">보유 캐쉬 : 100000 p</div>
         </div>
       </div>
-
-      {isModal && 
-         <Modal isOpen={isModal} toggle={modalToggle} centered={true}>
-         <ModalHeader toggle={modalToggle}>Clnewze</ModalHeader>
-         <ModalBody>
-           <h1>구독하시겠습니까?</h1>
-
-           <Button>Yes</Button>
-           <Button>No</Button>
-         </ModalBody>
-         <ModalFooter>
-            CLNEWZE musicion by clean.Dev
-         </ModalFooter>
-       </Modal>
-      }
     </div>
   );
 };
