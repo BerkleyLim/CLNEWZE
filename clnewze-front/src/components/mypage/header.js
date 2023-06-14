f20457de87b56e27c0311469b292ed7c468e23fb
berkleylim <berkleylim16@gmail.com>
2023-06-14 22:45:14 +0900

    feat: Update Mypage's Header
    구독하기 버튼 추가

--- clnewze-front/src/components/mypage/header.js
+++ clnewze-front/src/components/mypage/header.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./mypage.scss";
-import { Button } from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 const MypageHeader = () => {
+  const [isModal, setIsModal] = useState(false);
+
+  const subscriptModalToggle = () => setIsModal(!isModal);
+
+  const subscriptViewModal = () => {
+    setIsModal(!isModal);
+  }
+
   return (
     <div className="mypage-header">
       {/* <!-- 사진 --> */}
@@ -15,7 +23,10 @@ const MypageHeader = () => {
       {/* <!-- 마이페이지 정보 (커버랑 같이 겹치기) --> */}
       <div className="profile-information">
         <div className="profile-img">
-          <img src="/image/file/mypage/defalut-my-profile.png" className="img-size" />
+          <img
+            src="/image/file/mypage/defalut-my-profile.png"
+            className="img-size"
+          />
         </div>
         <div className="profile-component1">
           <div className="profile-name">홍길동</div>
@@ -24,11 +35,30 @@ const MypageHeader = () => {
           </div>
         </div>
         <div className="profile-component2">
-          <div className=""><Button>충전하기</Button></div>
+          <div className="">
+            <Button>충전하기</Button>
+            <Button onClick={() => subscriptViewModal()}>구독하기</Button>
+          </div>
           <div className="profile-return">환전 포인트 : 100000 p</div>
           <div className="profile-cache">보유 캐쉬 : 100000 p</div>
         </div>
       </div>
+      {isModal &&
+        <Modal isOpen={isModal} toggle={subscriptModalToggle} centered={true}>
+        <ModalHeader toggle={subscriptModalToggle}>구독하시겠습니까?</ModalHeader>
+        <ModalBody>
+          <Button color="primary">
+            구독하기
+          </Button>{' '}
+          <Button color="secondary">
+            취소하기
+          </Button>
+        </ModalBody>
+        <ModalFooter>
+          CLNEWZE musician by CleanDev.Inc
+        </ModalFooter>
+      </Modal>
+      }
     </div>
   );
 };
