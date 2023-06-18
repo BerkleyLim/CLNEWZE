ef5726f13568dc53974a21c4c532052d457b8d8e
berkleylim <berkleylim16@gmail.com>
2023-06-18 18:27:04 +0900

    refactor: Update header scss 타입으로 지정

--- clnewze-front/src/components/commom/header/index.js
+++ clnewze-front/src/components/commom/header/index.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./header.scss";
-// import styled from "styled-components";
+import styles from "./header.module.scss";
 import {
   Button,
   Modal,
   Nav,
-  // Dropdown,
-  // DropdownToggle,
-  // DropdownMenu,
-  // DropdownItem,
+  Navbar,
 } from "reactstrap";
 
 import SelectMenuIndex from "./dropdown/selectMemu/index";
@@ -36,14 +32,16 @@ const HeaderComponent = (props) => {
     }
   }
 
+  // console.log(styles)
   return (
     // < !--헤더 작성: 맴버, 소개, 곡목록, 악보, 커뮤니티, 다국어기능, 검색-- >
-    <div className="header">
+    // <div className="header">
+    <div className={`${styles?.header}`}>
       <Modal isOpen={loginIsModal} toggle={toggle} centered={true} >
         <LoginForm setLoginIsModal={setLoginIsModal} />
       </Modal>
-      <Nav className="hd-content1 navbar">
-        <div className="header_logo">
+      <Navbar className={`${styles?.hdContent1}`}>
+        <div className={`${styles?.header_logo}`}>
           <a href="/">
             로고(추가예정)
             <img src="" />
@@ -56,30 +54,15 @@ const HeaderComponent = (props) => {
         <div style={{ display: "flex", width: "100px" }}></div>
 
         <SelectLanguageIndex />
-        {/* <div className="header_language"> */}
-        {/* <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button"
-              id="select-language" data-target="#" data-toggle="dropdown"
-              aria-haspopup="true" role="button" aria-expanded="false">
-              Korean
-            </button>
-            <div className="dropdown-menu" role="menu" aria-labelledby="select-language">
-              <button id="select-korean" className="dropdown-item" type="button">Korean</button>
-              <button id="select-japanese" className="dropdown-item" type="button">Japanese</button>
-              <button id="select-English" className="dropdown-item" type="button">English</button>
-              <button id="" className="dropdown-item" type="button">etc..</button>
-            </div>
-          </div> */}
-        {/* </div> */}
 
         <i className="fas fa-list header-icon"></i>
 
-        <div className="header_mymenu">
+        <div className={`${styles?.header_mymenu}`}>
           <Button size="lg" onClick={userMenu}>
-            <span className="header-nickname">
+            <span className={`${styles?.headerNickname}`}>
               <PersonCircle/>
             </span>
-            <span className="header-nickname">
+            <span className={`${styles?.headerNickname}`}>
               {
                 user?.isLogin?
                 user.userNm :
@@ -89,20 +72,13 @@ const HeaderComponent = (props) => {
           </Button>
           <Button size="lg"><Alarm/></Button>
         </div>
-      </Nav>
+      </Navbar>
 
-      <Nav className="hd-content2 navbar">
+      <Navbar className={`${styles?.hdContent2}`}>
         <button type="button" onClick={() => movePage("introduction")}>
           {" "}
           소개
         </button>
-        {/* <button type="button" onClick={() => movePage("information")}>
-          곡정보
-        </button> */}
-        {/* 임시로 제거 - 차후 연구 필요 */}
-        {/* <button type="button" onClick={() => movePage("media")}>
-          영상
-        </button> */}
         <button type="button" onClick={() => movePage("sheetmusic")}>
           악보
         </button>
@@ -112,11 +88,7 @@ const HeaderComponent = (props) => {
         <button type="button" onClick={() => movePage("practiceRoom")}>
           연습실정보
         </button>
-        {/* 커뮤니티 게시판 제거 */}
-        {/* <button type="button" onClick={() => movePage("community")}>
-          커뮤니티
-        </button> */}
-      </Nav>
+      </Navbar>
     </div>
   );
 };
