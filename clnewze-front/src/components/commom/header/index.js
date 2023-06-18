a2568935c1f67f7dc8fb51121b60200713bba3df
berkleylim <berkleylim16@gmail.com>
2023-06-18 23:21:41 +0900

    feat: Create mobile's header
    모바일용 헤더 기능 추가

--- clnewze-front/src/components/commom/header/index.js
+++ clnewze-front/src/components/commom/header/index.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./header.module.scss";
-import {
-  Button,
-  Modal,
-  Nav,
-  Navbar,
-} from "reactstrap";
+import { Button, ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle, Modal, Navbar } from "reactstrap";
 
 import SelectMenuIndex from "./dropdown/selectMemu/index";
 import SelectLanguageIndex from "./dropdown/language/index";
 import LoginForm from "../../login/index";
 import { useSelector } from "react-redux";
-import { Alarm, PersonCircle } from "react-bootstrap-icons";
+import { Alarm, CardList, PersonCircle } from "react-bootstrap-icons";
 
 const HeaderComponent = (props) => {
   let navigate = useNavigate();
   const [loginIsModal, setLoginIsModal] = useState(false);
-  const toggle = () => setLoginIsModal(!loginIsModal);
-  const user = useSelector(state => state.user);
+  const [mobileIsDropDown, setMobileIsDropDown] = useState(false);
+  const modalToggle = () => setLoginIsModal(!loginIsModal);
+  const mobileIsDropDownToggle = () => setMobileIsDropDown(!mobileIsDropDown);
+  const user = useSelector((state) => state.user);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
   const movePage = (link) => {
     navigate(link);
@@ -28,16 +26,25 @@ const HeaderComponent = (props) => {
     if (user.isLogin) {
       window.location.href = "/mypage";
     } else {
-      toggle();
+      modalToggle();
     }
-  }
+  };
+
+  // 반응형 웹 적용
+  useEffect(() => {
+    const resizeListener = () => {
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", resizeListener);
+    // console.log(innerWidth);
+  }, [innerWidth]);
 
   // console.log(styles)
   return (
     // < !--헤더 작성: 맴버, 소개, 곡목록, 악보, 커뮤니티, 다국어기능, 검색-- >
     // <div className="header">
     <div className={`${styles?.header}`}>
-      <Modal isOpen={loginIsModal} toggle={toggle} centered={true} >
+      <Modal isOpen={loginIsModal} toggle={modalToggle} centered={true}>
         <LoginForm setLoginIsModal={setLoginIsModal} />
       </Modal>
       <Navbar className={`${styles?.hdContent1}`}>
@@ -47,48 +54,66 @@ const HeaderComponent = (props) => {
             <img src="" />
           </a>
         </div>
-        <SelectMenuIndex />
-        {/* <SelectMenuDropDown /> */}
+        {innerWidth >= 860 ? (
+          <>
+            <SelectMenuIndex />
+            {/* <SelectMenuDropDown /> */}
 
-        {/* <!-- 여기는 그냥 100px의 여백 두기 --> */}
-        <div style={{ display: "flex", width: "100px" }}></div>
+            {/* <!-- 여기는 그냥 100px의 여백 두기 --> */}
+            <div style={{ display: "flex", width: "100px" }}></div>
 
-        <SelectLanguageIndex />
+            <SelectLanguageIndex />
 
-        <i className="fas fa-list header-icon"></i>
+            <i className="fas fa-list header-icon"></i>
 
-        <div className={`${styles?.header_mymenu}`}>
-          <Button size="lg" onClick={userMenu}>
-            <span className={`${styles?.headerNickname}`}>
-              <PersonCircle/>
-            </span>
-            <span className={`${styles?.headerNickname}`}>
-              {
-                user?.isLogin?
-                user.userNm :
-                "로그인"
-              }
-            </span>
-          </Button>
-          <Button size="lg"><Alarm/></Button>
-        </div>
+            <div className={`${styles?.header_mymenu}`}>
+              <Button size="lg" onClick={userMenu}>
+                <span className={`${styles?.headerNickname}`}>
+                  <PersonCircle />
+                </span>
+                <span className={`${styles?.headerNickname}`}>
+                  {user?.isLogin ? user.userNm : "로그인"}
+                </span>
+              </Button>
+              <Button size="lg">
+                <Alarm />
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+          <ButtonDropdown isOpen={mobileIsDropDown} direction="down" toggle={mobileIsDropDownToggle}>
+      <DropdownToggle caret size="lg"><CardList /></DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem onClick={() => userMenu()}>{user?.isLogin ? user.userNm : "로그인"}</DropdownItem>
+        <DropdownItem onClick={() => movePage("introduction")}>소개</DropdownItem>
+        <DropdownItem onClick={() => movePage("sheetmusic")}>악보</DropdownItem>
+        <DropdownItem onClick={() => movePage("teacher")}>레스너 찾기</DropdownItem>
+        <DropdownItem onClick={() => movePage("practiceRoom")}>연습실 정보</DropdownItem>
+        {/* <DropdownItem>etc..</DropdownItem> */}
+      </DropdownMenu>
+    </ButtonDropdown>
+          </>
+        )}
       </Navbar>
 
-      <Navbar className={`${styles?.hdContent2}`}>
-        <button type="button" onClick={() => movePage("introduction")}>
-          {" "}
-          소개
-        </button>
-        <button type="button" onClick={() => movePage("sheetmusic")}>
-          악보
-        </button>
-        <button type="button" onClick={() => movePage("teacher")}>
-          레스너찾기
-        </button>
-        <button type="button" onClick={() => movePage("practiceRoom")}>
-          연습실정보
-        </button>
-      </Navbar>
+      {innerWidth >= 860 && (
+        <Navbar className={`${styles?.hdContent2}`}>
+          <button type="button" onClick={() => movePage("introduction")}>
+            {" "}
+            소개
+          </button>
+          <button type="button" onClick={() => movePage("sheetmusic")}>
+            악보
+          </button>
+          <button type="button" onClick={() => movePage("teacher")}>
+            레스너찾기
+          </button>
+          <button type="button" onClick={() => movePage("practiceRoom")}>
+            연습실정보
+          </button>
+        </Navbar>
+      )}
     </div>
   );
 };
