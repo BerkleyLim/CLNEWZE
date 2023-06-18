e339060b835b31bd481b7399b734baf5b592f2cc
berkleylim <berkleylim16@gmail.com>
2023-06-18 22:03:17 +0900

    style: Update mypage Login

--- clnewze-front/src/components/mypage/user/index.js
+++ clnewze-front/src/components/mypage/user/index.js
@@ -4,16 +4,14 @@ import { Col, Form, FormGroup, Input, Label } from "reactstrap";
 const UserInfomation = () => {
   return (
     <>
-          안녕하세요 저는 홍길동 입니다.
-      만나서 반갑습니다.
       <Form>
         <FormGroup row>
           <Col md={2}>
-            <Label for="email">Email</Label>
+            <Label for="exampleEmail">Email</Label>
           </Col>
           <Col md={10}>
             <Input
-              id="email"
+              id="exampleEmail"
               name="email"
               placeholder="with a placeholder"
               type="email"
@@ -22,53 +20,14 @@ const UserInfomation = () => {
         </FormGroup>
         <FormGroup row>
           <Col md={2}>
-            <Label for="password">Password</Label>
+            <Label for="examplePassword">Password</Label>
           </Col>
           <Col md={10}>
             <Input
-              id="password"
+              id="examplePassword"
               name="password"
               placeholder="password placeholder"
               type="password"
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Col md={2}>
-            <Label for="username">username</Label>
-          </Col>
-          <Col md={10}>
-            <Input
-              id="username"
-              name="username"
-              placeholder="username placeholder"
-              type="username"
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Col md={2}>
-            <Label for="nickname">nickname</Label>
-          </Col>
-          <Col md={10}>
-            <Input
-              id="nickname"
-              name="nickname"
-              placeholder="nickname placeholder"
-              type="nickname"
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Col md={2}>
-            <Label for="nickname">phone</Label>
-          </Col>
-          <Col md={10}>
-            <Input
-              id="phone"
-              name="phone"
-              placeholder="phone placeholder"
-              type="phone"
             />
           </Col>
         </FormGroup>
