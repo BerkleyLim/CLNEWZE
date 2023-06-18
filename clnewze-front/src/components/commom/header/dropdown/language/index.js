796a3fd53d1a594cd67b70c85e8610d5dce3b308
berkleylim <berkleylim16@gmail.com>
2023-06-18 18:32:26 +0900

    refactor: Update scss style by className

--- clnewze-front/src/components/commom/header/dropdown/language/index.js
+++ clnewze-front/src/components/commom/header/dropdown/language/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SelectLanguageDropdown from "./dropdown";
+import styles from "../../header.module.scss";
 
 const LanguageIndex = () => {
   return (
-    <div className="header_language">
+    <div className={`${styles?.header_language}`}>
       <SelectLanguageDropdown />
     </div>
   );
--- clnewze-front/src/components/commom/header/dropdown/selectMemu/index.js
+++ clnewze-front/src/components/commom/header/dropdown/selectMemu/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button, Input, InputGroup } from "reactstrap";
 import SelectMenuDropdown from "./dropdown";
+import styles from "../../header.module.scss";
 
 const SelectMenuIndex = () => {
   return (
-    <div className="header_search">
+    <div className={`${styles?.header_search}`}>
       <InputGroup>
         <SelectMenuDropdown />
         <Input className="form-control" />
