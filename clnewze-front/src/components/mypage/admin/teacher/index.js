4e361bf281655f7e457494d66cbd9ba56e5d79e6
berkleylim <berkleylim16@gmail.com>
2023-07-02 22:17:46 +0900

    feat: Update Admin : Teacher
    레스너 찾기 메뉴 드래그앤 드롭 기능 추가

--- clnewze-front/src/components/mypage/admin/teacher/index.js
+++ clnewze-front/src/components/mypage/admin/teacher/index.js
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import update from "immutability-helper";
 import { Table } from "reactstrap";
-import DataTable from "./menu/dataTable"
-import URI from "../../../util/URI"
+import DataTable from "./menu/dataTable";
+import URI from "../../../util/URI";
 
 function AdminTeacher() {
   const [menuData, setMenuData] = useState();
+  const [isStateUpdate, setIsStateUpdate] = useState(false);
   useEffect(() => {
     URI.get(
       process.env.REACT_APP_API_ROOT + "/api/teacher/master/menu/category"
@@ -14,18 +18,46 @@ function AdminTeacher() {
       })
       .catch((e) => console.error(e));
   }, []);
+
+  // MenuData 드래그앤 드롭 기능 추가
+  const MenuDataDndMove = useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragCard = menuData[dragIndex];
+      setMenuData(
+        update(menuData, {
+          $splice: [
+            [dragIndex, 1], // delete
+            [hoverIndex, 0, dragCard], // Add
+          ],
+        })
+      )
+
+      // 여기서 전체 리스트 update API 삽입
+      setIsStateUpdate(!isStateUpdate);
+
+      // 삽입 끝
+    },
+    [menuData]
+  );
   return (
     <>
       <h1>메뉴 관리</h1>
       <Table striped>
         <thead>
           <tr>
-            <th>orderBy</th>
+            <th>index</th>
+            <th>prevOrderBy</th>
             <th>name</th>
             <th>category</th>
           </tr>
         </thead>
-        <DataTable menuData={menuData} />
+        <tbody>
+          {menuData?.map((data, index) => (
+            <DndProvider key={index} backend={HTML5Backend}>
+              <DataTable index={index+1} data={data} MenuDataDndMove={MenuDataDndMove}/>
+            </DndProvider>
+          ))}
+        </tbody>
       </Table>
       <h1>레스너 순서 관리</h1>
     </>
--- clnewze-front/src/components/mypage/admin/teacher/menu/dataTable.js
+++ clnewze-front/src/components/mypage/admin/teacher/menu/dataTable.js
@@ -1,16 +1,71 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
 
-function DataTable({ menuData }) {
+const ItemTypes = {
+  TABLE: 'table'
+}
+
+function DataTable({ data, index, MenuDataDndMove }) {
+    // drag and drop 관련
+  // 참조 : https://velog.io/@suyeonme/React-DragDrop-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+  // 문서 참조 : https://react-dnd.github.io/react-dnd/docs/api/use-drag
+  const ref = useRef(null); // (*)
+
+  const [, drop] = useDrop({
+    // (*)
+    accept: ItemTypes.TABLE,
+    hover(item, monitor) {
+      if (item.index === index)
+        return
+      if (!ref.current) {
+        return;
+      }
+
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      MenuDataDndMove(dragIndex, hoverIndex);
+
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    // (*)
+    item: { type: ItemTypes.TABLE, data, index },
+    type: ItemTypes.TABLE,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref)); // (*)
+  // drag and drop 끝
   return (
-    <tbody>
-      {menuData?.map((data, index) => 
-        <tr key={index}>
-          <th scope="row">{data?.orderby}</th>
-          <td>{data?.name}</td>
-          <td>{data?.category}</td>
-        </tr>
-      )}
-    </tbody>
+    <tr ref={ref} key={index}>
+      <th scope="row">{index}</th>
+      <th scope="row">{data?.orderby}</th>
+      <td>{data?.name}</td>
+      <td>{data?.category}</td>
+    </tr>
   );
 }
 
