7882a6f810202eae0d78e19a4af4a30ca79f07cf
berkleylim <berkleylim16@gmail.com>
2023-07-15 21:03:31 +0900

    refactor: Remove any code
    필요 없는 코드 제거 (일부 고도화) - css 부분 및 주석 제거

--- clnewze-front/src/components/practiceRoom/contents/board/index.js
+++ clnewze-front/src/components/practiceRoom/contents/board/index.js
@@ -37,50 +37,6 @@ const BoardIndex = ({ boards, onClickView }) => {
         </Col>
       ))}
     </Row>
-    // <>
-    //   {boards?.map((board, index) => (
-    //     <Card
-    //       key={index}
-    //       mb={3}
-    //       className={`${styles?.autor_wrap}`}
-    //       onClick={() => onClickView(board)}
-    //     >
-    //       <CardHeader className="board-title">{board?.title}</CardHeader>
-    //       <CardBody>
-    //         <Card mb={3} className={`${styles?.content}`}>
-    //           <Row className="no-gutters">
-    //             <Col md={1}>
-    //               <img
-    //                 src={board?.img}
-    //                 className="card-img"
-    //                 alt="..."
-    //                 style={{ width: "150px", height: "150px" }}
-    //               />
-    //             </Col>
-    //             <Col md={11}>
-    //               <Row className="card-text">
-    //                 <Col md={1} />● 성함 : {board?.name}
-    //               </Row>
-    //               <Row className="card-text">
-    //                 <Col md={1} />● 위치 : {board?.place}
-    //               </Row>
-    //               <Row className="card-text">
-    //                 <Col md={1} />● 연습실 정보 : {board?.categories}
-    //               </Row>
-    //               <Row className="card-text">
-    //                 <Col md={1} />● 대여 가격 : {board?.price}
-    //               </Row>
-
-    //               <div className="card-text">
-    //                 <small className="text-muted">{board?.upLoadDate}</small>
-    //               </div>
-    //             </Col>
-    //           </Row>
-    //         </Card>
-    //       </CardBody>
-    //     </Card>
-    //   ))}
-    // </>
   );
 };
 
