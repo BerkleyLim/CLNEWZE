38cc1a3efcfab970a0d33388399f27ce4d43247c
berkleylim <berkleylim16@gmail.com>
2023-08-14 15:37:41 +0900

    fix: Front-End> 화면단, 페이지네이션 기능 추가 - 폴더 구조 개편 - 페이지네이션 개편 추가 (사용자 편의성) - Back-End와의 통신으로 페이지 번호를 파라미터로 남김(이전페이지로 넘기기위한 작업 중 하나)

--- clnewze-front/src/components/blog/index.js
+++ clnewze-front/src/components/blog/index.js
@@ -1,28 +1,52 @@
 import React, { useEffect, useState } from "react";
-import {
-  Badge,
-  Card,
-  CardBody,
-  CardText,
-  CardTitle,
-  Row,
-} from "reactstrap";
 import styles from "./blog.module.scss";
 import URI from "../util/URI";
+import BlogList from './contents/index'
+import BoardPagingnation from "./contents/paging";
+import { useParams } from "react-router-dom";
 
 const BlogIndex = () => {
+  const param = useParams();
   const [blogData, setBlogData] = useState();
+  // Pagination 관련
+  const [page, setPage] = useState(param?.pageNo); // 페이지
+  const limit = 10;
+  const offset = (page - 1) * 10; // 시작점, 끝점 구하는 offset
+  const [pagingCount, setPagingCount] = useState();
+  // Pagination 관련 끝
+  const [isModal, setIsModal] = useState(false);
+  const [modalData, setModalData] = useState();
 
-  const BadgeColor = ['primary', 'secondary', 'success', 'danger', 'info'];
+  // const postsData = (posts) => {
+  //   if (!!posts) {
+  //     let result = posts?.slice(offset, offset + limit);
+  //     return result;
+  //   }
+  // };
 
   useEffect(() => {
     URI.get(
       process.env.REACT_APP_API_ROOT +
-        "/api/blog/selectList"
+        "/api/blog/selectList" 
+          + "?pageNo=" + offset
+          + "&limit=" + limit
     )
       .then((res) => setBlogData(res.data.data))
       .catch((e) => console.error(e));
+  }, [offset])
+
+  console.log(offset)
+
+  useEffect(() => {
+    URI.get(
+      process.env.REACT_APP_API_ROOT + 
+        "/api/blog/selectListAllCount"
+    )
+      .then((res) => setPagingCount(res.data.data))
+      .catch((e) => console.error(e))
   }, [])
+
+  console.log(page)
 
   console.log(blogData)
 
@@ -35,94 +59,32 @@ const BlogIndex = () => {
       <div>현재 좋은 아이디어를 가지고 계시면 언제든지 피드백 환영합니다.</div>
       <div>이메일 문의 : berkleylim16@gmail.com.</div>
       <div>오픈채팅 : https://open.kakao.com/me/clnewzedeveloper</div>
-      {
-        blogData?.map((blog, index) => 
-          <Row key={index} className={"d-flex mb-3 mt-3"}>
-            <Card className={`${styles?.blogCard}`}>
-              <CardBody className={`${styles?.blogCardBody}`}>
-                <CardTitle className={`${styles?.blogCardTitle}`}>
-                  제목 : {blog?.title}
-                </CardTitle>
-                <CardText className={`${styles?.blogCardText}`}>
-                  내용 : {blog?.contents}
-                </CardText>
-                <CardText>
-                  {
-                    blog?.tag?.map((t,tIndex) => 
-                      <Badge
-                        key={tIndex}
-                        color={`${BadgeColor[tIndex%5]}`}
-                        pill={true}
-                        className={`m-2 ${styles?.blogBadgeTag}`}
-                      >
-                        {"# " + t}
-                      </Badge>
-                    )
-                  }
-                </CardText>
-              </CardBody>
-            </Card>
-          </Row>
-        )
-      }
-      {/* <Row className={"d-flex mb-3 mt-3"}>
-        <Card className={`${styles?.blogCard}`}>
-          <CardBody className={`${styles?.blogCardBody}`}>
-            <CardTitle className={`${styles?.blogCardTitle}`}>
-              제목 : 사랑하는 사람에게 들려주는 이야기
-            </CardTitle>
-            <CardText className={`${styles?.blogCardText}`}>
-              내용 : 저는 사랑하는 사람에게 달콤한 사랑을 주기 위해 시를 쓰는
-              노래를 들려주고 싶습니다. 내용 : 저는 사랑하는 사람에게 달콤한
-              사랑을 주기 위해 시를 쓰는 노래를 들려주고 싶습니다. 내용 : 저는
-              사랑하는 사람에게 달콤한 사랑을 주기 위해 시를 쓰는 노래를
-              들려주고 싶습니다. 내용 : 저는 사랑하는 사람에게 달콤한 사랑을
-              주기 위해 시를 쓰는 노래를 들려주고 싶습니다. 내용 : 저는 사랑하는
-              사람에게 달콤한 사랑을 주기 위해 시를 쓰는 노래를 들려주고
-              싶습니다. 내용 : 저는 사랑하는 사람에게 달콤한 사랑을 주기 위해
-              시를 쓰는 노래를 들려주고 싶습니다. 내용 : 저는 사랑하는 사람에게
-              달콤한 사랑을 주기 위해 시를 쓰는 노래를 들려주고 싶습니다.
-            </CardText>
-            <CardText>
-              <Badge
-                color="primary"
-                pill={true}
-                className={`m-2 ${styles?.blogBadgeTag}`}
-              >
-                {"# 태그1"}
-              </Badge>
-              <Badge
-                color="secondary"
-                pill={true}
-                className={`m-2 ${styles?.blogBadgeTag}`}
-              >
-                {"# 태그2"}
-              </Badge>
-              <Badge
-                color="success"
-                pill={true}
-                className={`m-2 ${styles?.blogBadgeTag}`}
-              >
-                {"# 태그3"}
-              </Badge>
-              <Badge
-                color="danger"
-                pill={true}
-                className={`m-2 ${styles?.blogBadgeTag}`}
-              >
-                {"# 태그4"}
-              </Badge>
-              <Badge
-                color="info"
-                pill={true}
-                className={`m-2 ${styles?.blogBadgeTag}`}
-              >
-                {"# 태그5"}
-              </Badge>
-            </CardText>
-          </CardBody>
-        </Card>
-      </Row> */}
+
+      <div className={`${styles?.contain}`}>
+        {/* <Menu boards={boards} major={selectMajor} majormenu={majormenu} /> */}
+
+        <div>
+          <BlogList 
+            // blogData={postsData(blogData)}
+            blogData={blogData}
+            // onClickView={onClickView}
+            />
+
+
+          {/* 모달 상세 출력 */}
+          {/* <Modal isOpen={isModal} toggle={toggle} centered={true} size="xl">
+            <BoardForm teacher={modalData} />
+          </Modal> */}
+
+          <BoardPagingnation
+            limit={limit}
+            page={page}
+            // totalPosts={blogData?.length}
+            totalPosts={pagingCount}
+            setPage={setPage}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- clnewze-front/src/components/blog/tableIndex.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BlogTableIndex = () => {
-  return (
-    <div>BlogTableIndex</div>
-  )
-}
-
-export default BlogTableIndex
