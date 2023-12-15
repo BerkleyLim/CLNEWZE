3b360f29220eda1cad0036b98610c18fbe2285b8
berkleylim <berkleylim16@gmail.com>
2023-12-15 16:09:46 +0900

    fix: chart 부분 UI 라이브러리 교체 - Google Chart를 이용하여 작업 예정

--- clnewze-front/src/components/mypage/admin/dashboard/index.js
+++ clnewze-front/src/components/mypage/admin/dashboard/index.js
@@ -1,35 +1,50 @@
 import React, { useState } from 'react'
+import Chart from 'react-google-charts'
 
-import { recentSales } from './item/data/recentSales'
-import { salesByCountry } from './item/data/salesByCountry'
-import { salesByPerson } from './item/data/salesByPerson'
-import CustomNavBar from './CustomNavBar'
-import Gauge from './item/Gauge'
-import SalesChart from './item/SalesChart'
-import SalesPie from './item/SalesPie'
-import TransactionList from './item/TransactionList'
 
 
 // 참조 : https://stackblitz.com/edit/react-wijmo-salesdash-udsdv7?file=style.css
 const DashBoard = () => {
 
-  const calculateSales = () => {
-    let totalSales = 0;
-    recentSales.forEach(sale => totalSales += sale.value);
-    return totalSales;
-  }
-
   return (
     <div>
-        <CustomNavBar title="Sales Dashboard" />
-          <div className="row">
-            <Gauge data={calculateSales()} />
-            <SalesChart salesData={salesByCountry} />
-            <SalesPie salesData={salesByPerson} />
-          </div>
-          <div className="row">
-            <TransactionList transactions={recentSales} />
-          </div>
+      <h1>대쉬보드</h1>
+
+      <h2>매출액</h2>
+      <Chart 
+        chartType="ScatterChart"
+        data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
+
+      <h2>오늘 방문자 수</h2>
+      <Chart 
+        chartType="ScatterChart"
+        data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
+
+      <h2>악보 판매 건수</h2>
+      <Chart 
+        chartType="ScatterChart"
+        data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
+
+      <h2>악보 판매 건수</h2>
+      <Chart 
+        chartType="ScatterChart"
+        data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
     </div>
   )
 }
--- clnewze-front/src/components/mypage/admin/dashboard/item/data/salesByPerson.js
+++
@@ -1,6 +0,0 @@
-export const salesByPerson = [
-  { name: "Bill", sales: 5500},
-  { name: "Ted", sales: 7250},
-  { name: "Angela", sales: 8500},
-  { name: "Jill", sales: 10750}
-]
