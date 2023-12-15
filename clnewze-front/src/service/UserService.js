ef9d6b27d5d35d90cc67c19ea9107897492a071b
berkleylim <berkleylim16@gmail.com>
2023-12-16 01:38:48 +0900

    style: clean UserService.js - 필요 없는 코드 제거

--- clnewze-front/src/service/UserService.js
+++ clnewze-front/src/service/UserService.js
@@ -1,11 +1,6 @@
-import { useRecoilState } from 'recoil';
 import URI from '../util/URI'
-import { userState } from '../recoil/state/userState';
 
 const UserService = () => {
-  // 필요한 state 설정
-  const [user, setUser] = useRecoilState(userState);
-
   // 1) 로그인 여부 설정
   const isExistUserService = async (inputs) => {
     // 입력 (차후 복잡한 권한을 부여 받을 예정 => jwt 도입 예정)
