7e2cd62c5dff44a34bc1e95d78619f802d9b8085
berkleylim <berkleylim16@gmail.com>
2023-12-15 23:13:08 +0900

    refactor: API 호출 부분 분리 - UserService : API 분리 - UserContainer : API 호출하여 적용

--- clnewze-front/src/hooks/UserContainer.js
+++ clnewze-front/src/hooks/UserContainer.js
@@ -1,40 +1,18 @@
-import { useRecoilState, useResetRecoilState } from "recoil";
+import { useResetRecoilState } from "recoil";
 import { userState } from "../recoil/state/userState";
-import URI from '../util/URI'
+import UserService from "../service/UserService";
 
 // container 부분
 const UserContainer = () => {
-  const [user, setUser] = useRecoilState(userState);
   const logout = useResetRecoilState(userState);
+  const { isExistUserService } = UserService()
 
   // 로그인 처리 함수
   const handlerLogin = (inputs) => {
     // 입력 (차후 복잡한 권한을 부여 받을 예정 => jwt 도입 예정)
     // resolve 상태 : false 리턴 (모달 닫기, 사용자 페이지 회원 정보 접근 허용)
     // reject 상태 : true 리턴 (모달 유지, 사용자 페이지 회원 정보 접근 차단)
-    URI.post(process.env.REACT_APP_API_ROOT + "auth/simplelogin", {
-      id: inputs.id,
-      password: inputs.password,
-    })
-    .then((response) => {
-      if (response.data) {
-        alert("로그인 성공")
-        setUser({
-          ...user,
-          userId: "admin",
-          role_admin: "admin",
-          userNm: "admin",
-          isLogin: true
-        })
-        return false;
-      } else {
-        alert("로그인 실패")
-      }
-    })
-    .catch((e) => { 
-      console.error(e) 
-      return true;
-    });
+    return isExistUserService(inputs)
   };
 
   // 로그아웃 함수
