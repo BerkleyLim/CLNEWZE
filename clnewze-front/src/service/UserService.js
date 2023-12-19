908e8c39483ed50bcd681f7d9952fe0a5cbbed44
berkleylim <berkleylim16@gmail.com>
2023-12-19 18:05:40 +0900

    feat: To be maked auth of JWT restful API - 프론트엔드 restful API 제작 - JWT 인증 토큰 추가 - 추후, 백엔드 JWT restful api 구축 시 테스트 예정

--- clnewze-front/src/service/UserService.js
+++ clnewze-front/src/service/UserService.js
@@ -1,14 +1,16 @@
 import restApiAllUser from '../util/restApiAllUser'
+import restApiAuthJwt from '../util/restApiAuthJwt'
 
 const UserService = () => {
   // 1) 로그인 여부 설정
   const isExistUserService = async (inputs) => {
     // 입력 (차후 복잡한 권한을 부여 받을 예정 => jwt 도입 예정)
-    return await restApiAllUser.post(process.env.REACT_APP_API_ROOT + "auth/simplelogin", {
+    return await restApiAuthJwt.post(process.env.REACT_APP_API_ROOT + "auth/simplelogin", {
       id: inputs.id,
       password: inputs.password,
     })
     .then((response) => {
+      console.log(response.data)
       return response.data
     })
     .catch((e) => { 
@@ -16,6 +18,20 @@ const UserService = () => {
       return false
     });
   }
+  // const isExistUserService = async (inputs) => {
+  //   // 입력 (차후 복잡한 권한을 부여 받을 예정 => jwt 도입 예정)
+  //   return await restApiAllUser.post(process.env.REACT_APP_API_ROOT + "auth/simplelogin", {
+  //     id: inputs.id,
+  //     password: inputs.password,
+  //   })
+  //   .then((response) => {
+  //     return response.data
+  //   })
+  //   .catch((e) => { 
+  //     console.error(e) 
+  //     return false
+  //   });
+  // }
 
   return {
     isExistUserService,
