77636849f2956cfd4dd5f2b61c7a22d2fb3b121c
berkleylim <berkleylim16@gmail.com>
2023-12-22 15:00:33 +0900

    comment: WebConfig.java 설명 추가 - CORS 정책에 의한 차단을 일시적 허용을 위한 것으로 설정한 것을 알림

--- clnewze-back/src/main/java/com/clnewze/back/clnewzeback/configuration/WebConfig.java
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/configuration/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// 이 설정은 CORS 설정 때문에 지정한 API 표시
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
