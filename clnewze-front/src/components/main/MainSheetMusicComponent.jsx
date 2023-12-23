d196890f9551361fda502932a2c15b18f004aec1
berkleylim <berkleylim16@gmail.com>
2023-12-23 15:16:40 +0900

    refactor: main component 정리 - sheetmusic 불필요한 코드 제거 - customcard 디렉토리 제거 (필요 없으므로)

--- clnewze-front/src/components/main/MainSheetMusicComponent.jsx
+++ clnewze-front/src/components/main/MainSheetMusicComponent.jsx
@@ -3,17 +3,7 @@ import { ChevronRight } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
 import styles from "../../scss/main/main.module.scss";
 
-import {
-  Carousel,
-  CarouselCaption,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselItem,
-} from "reactstrap";
-import restApiAllUser from "../../util/restApiAllUser";
-import SheetMusic from "../sheetmusic/contents/board/SheetMusicListComponent";
 import styled from "styled-components";
-import SheetMusicContainer from "../../hooks/SheetMusicContainer";
 import SheetMusicService from "../../service/SheetMusicService";
 import { useRecoilState } from "recoil";
 import { sheetMusicState } from "../../recoil/state/sheetMusicState";
@@ -68,15 +58,11 @@ const Button = styled.button`
 
 const MainSheetMusicComponent = () => {
   const navigate = useNavigate();
-  // const [activeIndex, setActiveIndex] = useState(0);
-  // const [animating, setAnimating] = useState(false);
-  // const [items, setItems] = useState([]);
   const {sheetMusicSelectList} = SheetMusicService();
   const [sheetMusic, setSheetMusic] = useRecoilState(sheetMusicState);
 
   // 장르 설정 할때 마다
   useEffect(() => {
-    // request parameter : genre, pageNo, limit
     const fetchData = async () => {
       const data = await sheetMusicSelectList(null,0,10);
 
@@ -88,48 +74,8 @@ const MainSheetMusicComponent = () => {
 
     fetchData();
 
-    
-    // restApiAllUser
-    //   .get(process.env.REACT_APP_API_ROOT + "sheetmusic/selectList?genre=null")
-    //   .then((res) => setItems(res.data?.data))
-    //   .catch((e) => console.error(e));
   }, []);
 
-  // const next = () => {
-  //   if (animating) return;
-  //   const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-  //   setActiveIndex(nextIndex);
-  // };
-
-  // const previous = () => {
-  //   if (animating) return;
-  //   const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-  //   setActiveIndex(nextIndex);
-  // };
-
-  // const goToIndex = (newIndex) => {
-  //   if (animating) return;
-  //   setActiveIndex(newIndex);
-  // };
-
-  // const slides = items?.map((item, index) => {
-  //   return (
-  //     <CarouselItem
-  //       className="custom-tag"
-  //       tag="div"
-  //       key={index}
-  //       onExiting={() => setAnimating(true)}
-  //       onExited={() => setAnimating(false)}
-  //     >
-  //       <SheetMusic boards={items} />
-  //       <CarouselCaption
-  //         className="text-danger"
-  //         // captionText={item?.caption}
-  //         // captionHeader={item?.caption}
-  //       />
-  //     </CarouselItem>
-  //   );
-  // });
   return (
     <Container>
       {/* 참조 : https://velog.io/@071yoon/React-Horizontal-Scroll-%EA%B5%AC%ED%98%84 */}
@@ -149,39 +95,6 @@ const MainSheetMusicComponent = () => {
             )}
         </ScrollMenu>
     </Container>
-    // <>
-    //   <div className={`${styles?.mainCommonHeader}`}>
-    //     <h1>악보</h1>
-    //     <p onClick={() => navigate("/information")}>
-    //       <ChevronRight /> 더보기
-    //     </p>
-    //   </div>
-    //   <style>
-    //     {`.custom-tag {
-    //           max-width: 100%;
-    //           height: 500px;
-    //           background: black;
-    //         }`}
-    //   </style>
-    //   <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-    //     {/* <CarouselIndicators
-    //       items={!!items}
-    //       activeIndex={activeIndex}
-    //       onClickHandler={goToIndex}
-    //     /> */}
-    //     {slides}
-    //     <CarouselControl
-    //       direction="prev"
-    //       directionText="Previous"
-    //       onClickHandler={previous}
-    //     />
-    //     <CarouselControl
-    //       direction="next"
-    //       directionText="Next"
-    //       onClickHandler={next}
-    //     />
-    //   </Carousel>
-    // </>
   );
 };
 
--- clnewze-front/src/components/main/customcard/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CardIndex = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default CardIndex;
