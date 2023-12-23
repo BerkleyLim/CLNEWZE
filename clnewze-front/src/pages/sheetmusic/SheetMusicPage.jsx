3cd4df227be8b3df55283b5d265b277f26d5dd85
berkleylim <berkleylim16@gmail.com>
2023-12-23 16:15:26 +0900

    comment: rewrite comment by sheetmusic - 악보 페이지 부분 코멘트 수정

--- clnewze-front/src/pages/sheetmusic/SheetMusicPage.jsx
+++ clnewze-front/src/pages/sheetmusic/SheetMusicPage.jsx
@@ -12,17 +12,17 @@ const SheetMusicPage = (props) => {
   return (
     <div>
       <div className={`${styles?.contain}`}>
-        {/* 선생님 메뉴 선택 항목 */}
+        {/* 악보 메뉴 선택 항목 */}
         <SheetMusicMenuComponent />
 
         <div>
-          {/* 선생님 리스트 */}
+          {/* 악보 리스트 */}
           <SheetMusicListComponent />
 
-          {/* 선생님 모달 상세 출력 */}
+          {/* 악보 모달 상세 출력 */}
           <SheetMusicViewComponent />
 
-          {/* 선생님 페이지네이션 추가 */}
+          {/* 악보 페이지네이션 추가 */}
           <SheetMusicPagingNationComponent />
         </div>
       </div>
