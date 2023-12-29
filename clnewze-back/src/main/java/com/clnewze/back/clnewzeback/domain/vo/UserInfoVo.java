cc0ba6aa4b27c14dee030b7e94d8acbdf6b909c5
berkleylim <berkleylim16@gmail.com>
2023-12-29 14:00:52 +0900

    feat: UserInfoVo 생성 - 프론트엔드 개발자에게 뿌려줄 vo class 생성

---
+++ clnewze-back/src/main/java/com/clnewze/back/clnewzeback/domain/vo/UserInfoVo.java
@@ -0,0 +1,20 @@
+package com.clnewze.back.clnewzeback.domain.vo;
+
+import java.time.ZonedDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+// 프론트엔드 개발자에게 보낼 Entity 클래스
+@Data
+@AllArgsConstructor
+@Builder
+public class UserInfoVo {
+  private Long uno;
+  private String userName;
+  private String nickName;
+  private ZonedDateTime loginTime;
+  private String birthday;
+  private String activated;
+}
