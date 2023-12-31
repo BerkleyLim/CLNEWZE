176af3e4e4d53b892747d15e027567bb4c1bf0bf
berkleylim <berkleylim16@gmail.com>
2023-12-31 22:13:09 +0900

    fix: Display teacher view on main - 선생님 찾기 부분 메인 화면 표시 - 다음은 선생님 찾기에서만 출력함

--- clnewze-front/src/components/main/MainTeacherComponent.jsx
+++ clnewze-front/src/components/main/MainTeacherComponent.jsx
@@ -2,104 +2,82 @@ import React, { useEffect, useState } from "react";
 import { ChevronRight } from "react-bootstrap-icons";
 import { useNavigate } from "react-router-dom";
 import styles from "../../scss/main/main.module.scss";
+
+import { useRecoilState } from "recoil";
+import { teacherState } from "../../recoil/state/teacherState";
+import CustomHorizonScroll from "./item/CustomHorizonScroll";
 import {
-  Carousel,
-  CarouselCaption,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselItem,
+  Card,
+  CardBody,
+  CardFooter,
+  CardText,
+  CardTitle,
+  Col,
+  Row,
 } from "reactstrap";
-import UseApi from "../../util/UseApi";
-import Teacher from '../teacher/contents/TeacherListComponent';
+import TeacherService from "../../service/TeacherService";
 
 const MainTeacherComponent = () => {
   const navigate = useNavigate();
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-  const [items, setItems] = useState([]);
+  const { teacherSelectList } = TeacherService();
+  const [teacher, setTeacher] = useRecoilState(teacherState);
 
   // 장르 설정 할때 마다
   useEffect(() => {
-    UseApi.get(
-      process.env.REACT_APP_API_ROOT +
-        "teacher/selectList?major=null"
-    )
-      .then((res) => setItems(res.data?.data))
-      .catch((e) => console.error(e));
+    const fetchData = async () => {
+      const data = await teacherSelectList(null, 0, 10);
+
+      // error 나면 null 값 표시
+      if (!!data) {
+        setTeacher(data);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  };
-
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  };
-
-  const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  };
-
-  const slides = items?.map((item, index) => {
+  // 가로 스크롤용 component 추가
+  const TeacherComponent = ({data}) => {
     return (
-      <CarouselItem
-        className="custom-tag"
-        tag="div"
-        key={index}
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-      >
-          {/* <button type="button" className={`${styles?.commonMainCard}`} style={{backgroundColor:"white"}}>
-            1
-          </button> */}
-          <Teacher boards={items} />
-        <CarouselCaption
-          className="text-danger"
-          // captionText={item?.caption}
-          // captionHeader={item?.caption}
-        />
-      </CarouselItem>
+      <Row className={`${styles?.bootstrapRowAndColCenter}`}>
+        <Col className={`${styles?.bootstrapRowAndColCenter} mb-5`}>
+          <Card
+            style={{
+              width: "18rem",
+            }}
+            className={`${styles?.bootstrapRowAndColCenter}`}
+          >
+            <img
+              alt="Card"
+              className={`${styles?.imgCard}`}
+              src={data?.img}
+            />
+            {/* 조회수 : {data?.viewNumber} */}
+            <CardBody>
+            <CardTitle className={`${styles?.commonEllipsisTitle}`} tag="h5">{data?.title}</CardTitle>
+              <CardText className={`${styles?.commonEllipsisContent}`}>● 성함 : {data?.name}</CardText>
+              <CardText className={`${styles?.commonEllipsisContent}`}>● 위치 : {data?.place}</CardText>
+              <CardText className={`${styles?.commonEllipsisContent}`}>● 연습실 정보 : {data?.categories}</CardText>
+              <CardText className={`${styles?.commonEllipsisContent}`}>● 대여 가격 : {data?.price}</CardText>
+            </CardBody>
+            <CardFooter>{data?.upLoadDate}</CardFooter>
+          </Card>
+        </Col>
+      </Row>
     );
-  });
+  };
+
   return (
-    <>
+    <div>
+      {/* 참조 : https://velog.io/@071yoon/React-Horizontal-Scroll-%EA%B5%AC%ED%98%84 */}
       <div className={`${styles?.mainCommonHeader}`}>
-        <h1>레스너 찾기</h1>
+        <h1>선생님 찾기</h1>
         <p onClick={() => navigate("/teacher")}>
           <ChevronRight /> 더보기
         </p>
       </div>
-      <style>
-        {`.custom-tag {
-              max-width: 100%;
-              height: 500px;
-              background: black;
-            }`}
-      </style>
-      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-        {/* <CarouselIndicators
-          items={!!items}
-          activeIndex={activeIndex}
-          onClickHandler={goToIndex}
-        /> */}
-        {slides}
-        <CarouselControl
-          direction="prev"
-          directionText="Previous"
-          onClickHandler={previous}
-        />
-        <CarouselControl
-          direction="next"
-          directionText="Next"
-          onClickHandler={next}
-        />
-      </Carousel>
-    </>
+      <CustomHorizonScroll data={teacher} Component={TeacherComponent} />
+    </div>
   );
 };
 
