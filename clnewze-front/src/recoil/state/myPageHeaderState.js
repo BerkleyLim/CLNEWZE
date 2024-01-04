34a78e247d901b104d229481438732698a35f467
berkleylim <berkleylim16@gmail.com>
2024-01-04 21:05:59 +0900

    fix: remove recoil session - 새로고침 시 값 유지 하는 기능 제거 - 프론트단에 세션 수가 많아지면 서버에 영향을 미치므로 가급적 필요없는 세션에 저장하는거 제거함 - 추가적으로 사용하지 않는 코드도 주석 처리 - session 저장도 꼭 필요한 새로고침 유지할 때만 쓴다.

--- clnewze-front/src/recoil/state/myPageHeaderState.js
+++ clnewze-front/src/recoil/state/myPageHeaderState.js
@@ -1,14 +1,14 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+// import { recoilPersist } from "recoil-persist";
 
-const sessionStorage = 
-      typeof window !== 'undefined' ? window.sessionStorage : undefined
+// const sessionStorage = 
+//       typeof window !== 'undefined' ? window.sessionStorage : undefined
 
-const { persistAtom } = recoilPersist({
-  key: 'recoil-persist', // this key is using to store data in local storage
-  storage: sessionStorage, // configure which storage will be used to store the data
-  converter: JSON // configure how values will be serialized/deserialized in storage
-});
+// const { persistAtom } = recoilPersist({
+//   key: 'recoil-persist', // this key is using to store data in local storage
+//   storage: sessionStorage, // configure which storage will be used to store the data
+//   converter: JSON // configure how values will be serialized/deserialized in storage
+// });
 
 // 마이페이지 - 메뉴 클릭 관리
 export const myPageMenuState = atom({
--- clnewze-front/src/recoil/state/practiceRoomState.js
+++ clnewze-front/src/recoil/state/practiceRoomState.js
@@ -1,19 +1,19 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
+// import { recoilPersist } from "recoil-persist";
 
-const sessionStorage = 
-      typeof window !== 'undefined' ? window.sessionStorage : undefined
+// const sessionStorage = 
+//       typeof window !== 'undefined' ? window.sessionStorage : undefined
 
-const { persistAtom } = recoilPersist({
-  key: 'recoil-persist', // this key is using to store data in local storage
-  storage: sessionStorage, // configure which storage will be used to store the data
-  converter: JSON // configure how values will be serialized/deserialized in storage
-});
+// const { persistAtom } = recoilPersist({
+//   key: 'recoil-persist', // this key is using to store data in local storage
+//   storage: sessionStorage, // configure which storage will be used to store the data
+//   converter: JSON // configure how values will be serialized/deserialized in storage
+// });
 
 export const practiceRoomState = atom({
   key: 'practiceRoom',
   default: [],
-  effects_UNSTABLE: [persistAtom]
+  // effects_UNSTABLE: [persistAtom]
 });
 
 export const practiceRoomModalDataState = atom({
