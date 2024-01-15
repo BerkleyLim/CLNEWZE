0a8bcf5592dcafd8616455e8fc238a97d39e010c
berkleylim <berkleylim16@gmail.com>
2024-01-15 11:37:10 +0900

    feat: Add mypage Userinfo view - 회원정보 폼 추가 - 퍼블리싱만 작업 완료 - 2차 퍼블리싱 작업 예정

--- clnewze-front/src/components/mypage/user/info/UserInfoComponent.jsx
+++ clnewze-front/src/components/mypage/user/info/UserInfoComponent.jsx
@@ -1,21 +1,75 @@
-import React from 'react'
-import { Input } from 'reactstrap'
+import React from "react";
+import { Input, InputGroup, Label } from "reactstrap";
+import styles from '../../../../scss/mypage/user/info/user.module.scss'
 
 const UserInfoComponent = () => {
   return (
-    <div>
-      <div>조금 더 조사하고, 무엇이 들어가야 할지 정하기</div>
-      <div>Back-End DB 구성 확실하게 하고나서 정하기</div>
-      <div>ID</div>
-      <Input type="text" name="id"/>
-      <div>Password</div>
-      <div>이름</div>
-      <div>닉네임</div>
-      <div>생년월일</div>
-      <div>성별</div>
-      <div>성별</div>
+    <div className={`${styles?.mypageUserInfoForm}`}>
+      {/* 아이디 */}
+      <InputGroup>
+        <div style={{ width: "15%" }}>
+          <Label>ID</Label>
+        </div>
+        <div style={{ width: "85%" }}>
+          <Input type="text" name="id" />
+        </div>
+      </InputGroup>
+      {/* 비밀번호 */}
+      <InputGroup>
+        <div style={{ width: "15%" }}>
+          <Label>Password</Label>
+        </div>
+        <div style={{ width: "85%" }}>
+          <Input type="password" name="password" />
+        </div>
+      </InputGroup>
+      {/* 이름 */}
+      <InputGroup>
+        <div style={{ width: "15%" }}>
+          <Label>이름</Label>
+        </div>
+        <div style={{ width: "85%" }}>
+          <Input type="text" name="userName" />
+        </div>
+      </InputGroup>
+      {/* 닉네임 */}
+      <InputGroup>
+        <div style={{ width: "15%" }}>
+          <Label>닉네임</Label>
+        </div>
+        <div style={{ width: "85%" }}>
+          <Input type="text" name="nickName" />
+        </div>
+      </InputGroup>
+      {/* 생년월일 */}
+      <InputGroup>
+        <div style={{ width: "15%" }}>
+          <Label>생년월일</Label>
+        </div>
+        <div style={{ width: "85%" }}>
+          <Input type="text" name="birthday" />
+        </div>
+      </InputGroup>
+      {/* 성별 */}
+      <InputGroup>
+        <div style={{ width: "15%" }}>
+          <Label>성별</Label>
+        </div>
+        <div style={{ width: "85%" }}>
+          <Input type="text" name="sex" />
+        </div>
+      </InputGroup>
+      {/* 소개 */}
+      <InputGroup>
+        <div style={{ width: "15%" }}>
+          <Label>소개</Label>
+        </div>
+        <div style={{ width: "85%" }}>
+          <Input type="textarea" name="introduction" />
+        </div>
+      </InputGroup>
     </div>
-  )
-}
+  );
+};
 
-export default UserInfoComponent
+export default UserInfoComponent;
