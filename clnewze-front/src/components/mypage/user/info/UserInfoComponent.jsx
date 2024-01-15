af5b7963deabbbefb68456aff8a2a5504f8959f3
berkleylim <berkleylim16@gmail.com>
2024-01-15 12:33:33 +0900

    fix: 회원정보 폼 수정 - 먼저, 회원정보의 대한 반응형 기준으로 수정 - 차후, 너비가 낮을때 재수정 필요

--- clnewze-front/src/components/mypage/user/info/UserInfoComponent.jsx
+++ clnewze-front/src/components/mypage/user/info/UserInfoComponent.jsx
@@ -7,64 +7,64 @@ const UserInfoComponent = () => {
     <div className={`${styles?.mypageUserInfoForm}`}>
       {/* 아이디 */}
       <InputGroup>
-        <div style={{ width: "15%" }}>
+        <div style={{ width: "25%" }}>
           <Label>ID</Label>
         </div>
-        <div style={{ width: "85%" }}>
+        <div style={{ width: "75%" }}>
           <Input type="text" name="id" />
         </div>
       </InputGroup>
       {/* 비밀번호 */}
       <InputGroup>
-        <div style={{ width: "15%" }}>
+        <div style={{ width: "25%" }}>
           <Label>Password</Label>
         </div>
-        <div style={{ width: "85%" }}>
+        <div style={{ width: "75%" }}>
           <Input type="password" name="password" />
         </div>
       </InputGroup>
       {/* 이름 */}
       <InputGroup>
-        <div style={{ width: "15%" }}>
+        <div style={{ width: "25%" }}>
           <Label>이름</Label>
         </div>
-        <div style={{ width: "85%" }}>
+        <div style={{ width: "75%" }}>
           <Input type="text" name="userName" />
         </div>
       </InputGroup>
       {/* 닉네임 */}
       <InputGroup>
-        <div style={{ width: "15%" }}>
+        <div style={{ width: "25%" }}>
           <Label>닉네임</Label>
         </div>
-        <div style={{ width: "85%" }}>
+        <div style={{ width: "75%" }}>
           <Input type="text" name="nickName" />
         </div>
       </InputGroup>
       {/* 생년월일 */}
       <InputGroup>
-        <div style={{ width: "15%" }}>
+        <div style={{ width: "25%" }}>
           <Label>생년월일</Label>
         </div>
-        <div style={{ width: "85%" }}>
+        <div style={{ width: "75%" }}>
           <Input type="text" name="birthday" />
         </div>
       </InputGroup>
       {/* 성별 */}
       <InputGroup>
-        <div style={{ width: "15%" }}>
+        <div style={{ width: "25%" }}>
           <Label>성별</Label>
         </div>
-        <div style={{ width: "85%" }}>
+        <div style={{ width: "75%" }}>
           <Input type="text" name="sex" />
         </div>
       </InputGroup>
       {/* 소개 */}
       <InputGroup>
-        <div style={{ width: "15%" }}>
+        <div style={{ width: "25%" }}>
           <Label>소개</Label>
         </div>
-        <div style={{ width: "85%" }}>
+        <div style={{ width: "75%" }}>
           <Input type="textarea" name="introduction" />
         </div>
       </InputGroup>
