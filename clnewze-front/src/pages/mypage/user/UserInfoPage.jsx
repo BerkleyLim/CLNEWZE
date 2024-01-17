ea072e1746f095c707805a3e52fc82ce8bd570ea
berkleylim <berkleylim16@gmail.com>
2024-01-17 22:07:10 +0900

    feat: add reactive web&app - 회원정보 반응형 웹앱 적용

--- clnewze-front/src/pages/mypage/user/UserInfoPage.jsx
+++ clnewze-front/src/pages/mypage/user/UserInfoPage.jsx
@@ -2,12 +2,21 @@ import React, { useState } from "react";
 import UserLoginForm from "../../../components/mypage/user/login/UserLoginForm";
 import styles from "../../../scss/mypage/user/info/user.module.scss";
 import UserInfoComponent from "../../../components/mypage/user/info/UserInfoComponent";
+import { useRecoilValue } from "recoil";
+import { innerWidthState } from "../../../recoil/state/commonState";
 
 const UserInfoPage = () => {
   // state 부
   const [isView, setIsView] = useState(true); // true : 보여줌, false : 로그인 상태 유지
+  const innerWidth = useRecoilValue(innerWidthState);
   return (
-    <div className={`${styles?.myPageWebContainer}`}>
+    <div
+      className={`${
+        innerWidth >= 860
+          ? styles?.myPageWebContainer
+          : styles?.myPageMobileContainer
+      }`}
+    >
       <h1>회원 정보</h1>
       <br />
       <br />
@@ -15,7 +24,7 @@ const UserInfoPage = () => {
         <UserInfoComponent />
       ) : (
         <div className={`${styles?.componentCenterAlign}`}>
-          <UserLoginForm setIsView={setIsView}/>
+          <UserLoginForm setIsView={setIsView} />
         </div>
       )}
     </div>
