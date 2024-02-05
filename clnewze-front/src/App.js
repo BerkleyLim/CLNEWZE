f1e1793590d9e2a69e3639966ec0c3ade63ffa32
berkleylim <berkleylim16@gmail.com>
2024-02-05 17:58:04 +0900

    feat: add UI on sign on page - 회원가입 폼 추가

--- clnewze-front/src/App.js
+++ clnewze-front/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import MyPage from "./pages/mypage/MyPage";
 import MainPage from "./pages/main/MainPage";
+import SignOnPage from "./pages/signon/SignOnPage";
 
 function App() {
   return (
@@ -9,11 +10,18 @@ function App() {
     <div>
       <Router>
         {/* 경로의 따라 view 기본 셋팅 조정 */}
-        {!window.location.href.includes("/mypage") && !window.location.href.includes("/admin") ? (
-          <MainPage />
-        ) : (
-          <MyPage />
-        )}
+        {/* mypage/admin , main, 로그인/회원가입정보 */}
+
+        {
+          window.location.href.includes("/signon") ?
+          <SignOnPage /> :
+            !window.location.href.includes("/mypage") && !window.location.href.includes("/admin") ? (
+              <MainPage />
+            ) : (
+              <MyPage />
+            )
+        }
+
       </Router>
     </div>
   );
